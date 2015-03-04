5806975eb10ba4059f4e67caa818062fd17a94e4
Steve Garton <sheepeatingtaz@gmail.com>
2015-03-04 09:27:06 +0000

    Added Date-to-date filter

--- django_filters/filters.py
+++ django_filters/filters.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from datetime import timedelta
+from datetime import timedelta, datetime, time
 
 
 from django import forms
@@ -222,6 +222,56 @@ class DateRangeFilter(ChoiceFilter):
         except (ValueError, TypeError):
             value = ''
         return self.options[value][1](qs, self.name)
+
+
+class DateRangeWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        attrs_from = {'class': 'date-from'}
+        attrs_to = {'class': 'date-to'}
+
+        if attrs:
+            attrs_from.update(attrs)
+            attrs_to.update(attrs)
+
+        widgets = (forms.TextInput(attrs=attrs_from), forms.TextInput(attrs=attrs_to))
+        super(DateRangeWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return [value.start, value.stop]
+        return [None, None]
+
+    def format_output(self, rendered_widgets):
+        return '<div class="date-range">' + ' - '.join(rendered_widgets) + '</div>'
+
+
+class DateRangeField(forms.MultiValueField):
+    widget = DateRangeWidget
+
+    def __init__(self, *args, **kwargs):
+        fields = (
+            forms.DateField(),
+            forms.DateField(),
+        )
+        super(DateRangeField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if data_list:
+            return slice(*data_list)
+        return None
+
+
+class DateToDateFilter(Filter):
+    field_class = DateRangeField
+
+    def filter(self, qs, value):
+        date_start = datetime.combine(value.start, time(0, 0, 0))
+        date_stop = datetime.combine(value.stop, time(23, 59, 59))
+
+        if value:
+            lookup = '%s__range' % self.name
+            return qs.filter(**{lookup: (date_start, date_stop)})
+        return qs
 
 
 class AllValuesFilter(ChoiceFilter):
